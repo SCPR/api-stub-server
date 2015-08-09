b868bbf8108d3a2568862b0355e32fd60d2ad606
Bryan Ricker <bryan.ricker@disney.com>
2015-08-08 23:57:52 -0700

    Update messaging

--- server.rb
+++ server.rb
@@ -5,7 +5,7 @@ bind_ip = Socket.ip_address_list.find(&:ipv4_private?).ip_address
 port = "5000"
 root = "./fixtures"
 
-puts ">>> Set your preroll URL to http://#{bind_ip}:#{port}/YourFixtureFile.xml"
+puts ">>> Point your app at: http://#{bind_ip}:#{port}/YourFixtureFile.xml"
 puts "ruby -run -e httpd -- -p #{port} -b #{bind_ip} #{root}\n\n"
 
 %W[ -p #{port} -b #{bind_ip} #{root} ].each { |a| ARGV << a }
